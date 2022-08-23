761ea333085d913b73e57db9d61dd4098b08be8e
A. Cooke <atc4@rice.edu>
2022-08-23 15:15:28 -0500

    Created alias file for fish

---
+++ config/fish/aliases.fish
@@ -0,0 +1,12 @@
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias booklist 'exa ~/Projects/Openstax/enki/ce-styles/styles/books'
+alias gf 'git fetch'
+alias gp 'git pull'
+alias jump cdh
+alias t tmux
+alias ta 'tmux attach -t'
+alias tls 'tmux ls'
+alias tn 'tmux new -t'
+alias v nvim
+alias vim nvim
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,18 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set fish_key_bindings fish_user_key_bindings
-    set -gx EDITOR nvim
-end
+# PATH Calls
+. ~/.config/fish/aliases.fish
 
+# Plugin Calls
 starship init fish | source
-
 thefuck --alias | source
+
+# VIM Mode
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
+
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
+end
